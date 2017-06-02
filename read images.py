eb13cac64f61a75cb16ce740340e3b46c576cc6f
Ajay <ajay@deonarine.com>
2017-06-02 16:28:21 -0400

    images

--- read images.py
+++ read images.py
@@ -7,6 +7,7 @@ import requests
 os.environ["KERAS_BACKEND"] = "tensorflow"
 
 from io import BytesIO
+from skimage import io, img_as_float
 from keras import backend as K
 
 import numpy as np
@@ -22,3 +23,21 @@ if os.path.exists("H:\image-app"):
 print(os.getcwd())
 
 dir_list = ['golden retriever']
+
+#To hold the directory, image ID and image array
+IMAGES = []
+for dir in dir_list:
+    os.chdir(dir)
+    df = pd.read_csv('URLs.csv')
+    print(df.shape)
+    for row in np.arange(5):
+    #for row in np.arange(df.shape[0]):
+        if row % 10 == 0:
+            print(row)
+        req = requests.get(df.loc[row, 'url'])
+        if req.ok:
+            img = io.imread(BytesIO(req.content))
+            IMAGES.append((dir, df.loc[row, 'ID'], img_as_float(img)))
+
+io.imsave('test.jpg', img)
+io.imsave('test2.jpg', img_as_float(img))
